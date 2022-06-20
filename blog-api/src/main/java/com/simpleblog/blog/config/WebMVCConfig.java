9095063230ac54e7dbcd204051e77270d72b6823
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-06-20 21:49:34 +1000

    refactored id

--- blog-api/src/main/java/com/simpleblog/blog/config/WebMVCConfig.java
+++ blog-api/src/main/java/com/simpleblog/blog/config/WebMVCConfig.java
@@ -2,17 +2,19 @@ package com.simpleblog.blog.config;
 
 import com.simpleblog.blog.handler.AuthInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
     @Autowired
     private AuthInterceptor authInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
     }
 
     @Override
