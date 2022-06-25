d3a0831b616f593a44331e562ba8ca8e82036942
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-06-25 14:34:28 +1000

    added an env variable

--- blog-api/src/main/java/com/simpleblog/blog/config/WebMVCConfig.java
+++ blog-api/src/main/java/com/simpleblog/blog/config/WebMVCConfig.java
@@ -3,6 +3,7 @@ package com.simpleblog.blog.config;
 import com.simpleblog.blog.handler.AuthInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,9 +13,12 @@ public class WebMVCConfig implements WebMvcConfigurer {
     @Autowired
     private AuthInterceptor authInterceptor;
 
+    @Autowired
+    private Environment env;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").allowedOrigins(env.getProperty("allowedOrigins"));
     }
 
     @Override
